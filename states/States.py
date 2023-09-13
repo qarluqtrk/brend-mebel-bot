c33520976e6e89b913a36adc4cf3ad2a88f2f379
qarluqturk <umidjonuchqunugli@hotmail.com>
2023-09-13 15:12:22 +0500

    Initial commit

---
+++ states/States.py
@@ -0,0 +1,12 @@
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+
+class Auth(StatesGroup):
+    name = State()
+    phone_number = State()
+    location = State()
+
+
+class OrderCustom(StatesGroup):
+    type = State()
+    size = State()
